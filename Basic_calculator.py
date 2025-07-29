ecbaf46f3de4a21b1fe6ca776d32fdd8277b4d96
Upendo1 <upendochacha@gmail.com>
2025-07-29 21:43:46 +0300

    Added Basic_calculator.py

---
+++ Basic_calculator.py
@@ -0,0 +1,13 @@
+#Basic_Calculator
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+#Results
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")  
+print(f"Difference: {difference_result}") 
+print(f"Product: {product_result}")  
+print(f"Quotient: {quotient_result}") 
